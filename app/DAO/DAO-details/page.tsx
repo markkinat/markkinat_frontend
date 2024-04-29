5c618ef0ee7e3fcdfd7753779dca6ac8985c9e16
Mona Lisarogue-in-anarchy <nehemiaheje@gmail.com>
2024-04-29 18:04:31 +0100

    DOA implementation

---
+++ app/DAO/DAO-details/page.tsx
@@ -0,0 +1,285 @@
+const DAODetails = () => {
+  return (
+    <>
+      <div className="container mt-24">
+        <div className="p-10 flex">
+          <div className="flex flex-col gap-8 my-5 p-5 text-neutral-100">
+            <div className="flex gap-2 items-center">
+              <div className="flex items-center">
+                <div className="mr-">
+                  <p className="bg-[#68CE78] rounded-xl px-4">Active</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h3 className="text-2xl font-bold">
+                #32 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                ad minim veniam, quis nostrud exercitation ullamco nisi ut
+                aliquip ex ea commodo consequat.
+              </h3>
+
+              <div className="flex items-center justify-between">
+                <div className="flex gap-2 p-2 items-center">
+                  <div className="w-7 h-7 rounded-full bg-neutral-500"></div>
+                  <p>Web3BridgeDAODetails</p>
+                  <p className="border-[0.5px] border-neutral-700 rounded-xl px-2">
+                    core
+                  </p>
+                </div>
+                <div className="flex">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M12 5L11.6464 4.64645L12 4.29289L12.3536 4.64645L12 5ZM12.5 14C12.5 14.2761 12.2761 14.5 12 14.5C11.7239 14.5 11.5 14.2761 11.5 14L12.5 14ZM6.64645 9.64645L11.6464 4.64645L12.3536 5.35355L7.35355 10.3536L6.64645 9.64645ZM12.3536 4.64645L17.3536 9.64645L16.6464 10.3536L11.6464 5.35355L12.3536 4.64645ZM12.5 5L12.5 14L11.5 14L11.5 5L12.5 5Z"
+                      fill="#FFFFFF"
+                    />
+                    <path
+                      d="M5 16L5 17C5 18.1046 5.89543 19 7 19L17 19C18.1046 19 19 18.1046 19 17V16"
+                      stroke="#FFFFFF"
+                    />
+                  </svg>
+
+                  <h3 className="px-2">share</h3>
+                  <h3>...</h3>
+                </div>
+              </div>
+              <h3 className="">Proposal Category: Treasury Stewardship</h3>
+              <h3 className="">Adekunle Stephen - Team Lead</h3>
+              <h3 className="">Apollo - Co-lead</h3>
+              <h3 className="">Okekunle - Senior Dev</h3>
+              <p className="mt-2">
+                Proposal Category: <br /> Sed ut perspiciatis unde omnis iste
+                natus error sit voluptatem accusantium doloremque laudantium,
+                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis
+                et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+                fugit, sed quia consequuntur magni dolores eos qui ratione
+                voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+                ipsum quia dolor sit amet, consectetur, adipisci velit,
+              </p>
+            </div>
+
+            <div className="gap-2 p-6 border-[0.5px] border-neutral-700 rounded-lg mt-9">
+              <div className="flex">
+                <h3 className="text-white font-bold text-xl">Cast Your Vote</h3>
+              </div>
+              <div className="mt-3">
+                <div className="w-full p-2 rounded-3xl border-[0.5px] border-neutral-700 px-6">
+                  For
+                </div>
+              </div>
+              <div className="mt-3">
+                <div className="w-full p-2 rounded-3xl border-[0.5px] border-neutral-700 px-6">
+                  Against
+                </div>
+              </div>
+              <div className="mt-3">
+                <div className="w-full p-2 rounded-3xl border-[0.5px] border-neutral-700 px-6">
+                  Abstain
+                </div>
+              </div>
+              <div className="mt-4">
+                <button className="w-full p-2 rounded-3xl border-[0.5px] bg-white text-black font-bold">
+                  Vote
+                </button>
+              </div>
+            </div>
+
+            <div className="gap-2 p-6 border-[0.5px] border-neutral-700 rounded-lg mt-9">
+              <div className="flex">
+                <h3 className="text-white font-bold text-xl">Votes</h3>
+              </div>
+              <div className="mt-3"></div>
+              <div className="mt-4">
+                <div className="flex gap-2 p-2 items-center justify-between">
+                  <div className="flex">
+                    <div className="w-7 h-7 rounded-full bg-neutral-500 gap-2"></div>
+                    <p>Web3BridgeDAODetails</p>
+                  </div>
+                  <div>
+                    <h3>For</h3>
+                  </div>
+                  <div>
+                    <h3>1K VOTES</h3>
+                  </div>
+                </div>
+                <div className="flex gap-2 p-2 items-center justify-between">
+                  <div className="flex">
+                    <div className="w-7 h-7 rounded-full bg-neutral-500 gap-2"></div>
+                    <p>Web3BridgeDAODetails</p>
+                  </div>
+                  <div>
+                    <h3>For</h3>
+                  </div>
+                  <div>
+                    <h3>1K VOTES</h3>
+                  </div>
+                </div>
+                <div className="flex gap-2 p-2 items-center justify-between">
+                  <div className="flex">
+                    <div className="w-7 h-7 rounded-full bg-neutral-500 gap-2"></div>
+                    <p>Web3BridgeDAODetails</p>
+                  </div>
+                  <div>
+                    <h3>For</h3>
+                  </div>
+                  <div>
+                    <h3>1K VOTES</h3>
+                  </div>
+                </div>
+                <div className="flex gap-2 p-2 items-center justify-between">
+                  <div className="flex">
+                    <div className="w-7 h-7 rounded-full bg-neutral-500 gap-2"></div>
+                    <p>Web3BridgeDAODetails</p>
+                  </div>
+                  <div>
+                    <h3>For</h3>
+                  </div>
+                  <div>
+                    <h3>1K VOTES</h3>
+                  </div>
+                </div>
+                <div className="flex gap-2 p-2 items-center justify-between">
+                  <div className="flex">
+                    <div className="w-7 h-7 rounded-full bg-neutral-500 gap-2"></div>
+                    <p>Web3BridgeDAODetails</p>
+                  </div>
+                  <div>
+                    <h3>For</h3>
+                  </div>
+                  <div>
+                    <h3>1K VOTES</h3>
+                  </div>
+                </div>
+
+                <div className="w-full p-2 rounded-3xl border-[0.5px] border-neutral-700 text-center mt-2">
+                  View all
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="gap-2 p-6 border-[0.5px] border-neutral-700 w-96 rounded-lg">
+              <div className="flex">
+                <h3 className="text-white font-bold text-xl">Sales live</h3>
+              </div>
+              <div className="flex justify-between mt-3">
+                <h3>Strategies</h3>
+                <div className="w-7 h-7 rounded-full bg-neutral-500"></div>
+              </div>
+              <div className="flex justify-between mt-2">
+                <h3>IPFS</h3>
+                <div className="flex items-center">
+                  <h3 className="text-white font-semi-bold px-1">#bafkrel</h3>
+
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M14 7.77778V15H1V2H7.5"
+                      stroke="white"
+                      stroke-width="1.5"
+                    />
+                    <path
+                      d="M15 1V0.25H15.75V1H15ZM6.53033 10.5303C6.23744 10.8232 5.76256 10.8232 5.46967 10.5303C5.17678 10.2374 5.17678 9.76256 5.46967 9.46967L6.53033 10.5303ZM14.25 5.64286V1H15.75V5.64286H14.25ZM15 1.75H9.64286V0.25H15V1.75ZM15.5303 1.53033L6.53033 10.5303L5.46967 9.46967L14.4697 0.46967L15.5303 1.53033Z"
+                      fill="white"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="flex justify-between mt-2">
+                <h3>Voting System</h3>
+                <h3 className="text-white font-semi-bold">
+                  Single choice voting
+                </h3>
+              </div>
+              <div className="flex justify-between mt-2">
+                <h3>Start date</h3>
+                <h3 className="text-white font-semi-bold">
+                  Apr 17, 2024, 7:10 PM
+                </h3>
+              </div>
+              <div className="flex justify-between mt-2">
+                <h3>End date</h3>
+                <h3 className="text-white font-semi-bold">
+                  Apr 17, 2024, 7:10 PM
+                </h3>
+              </div>
+              <div className="flex justify-between mt-2">
+                <h3>Snapshot</h3>
+                <div className="flex items-center">
+                  <h3 className="text-white font-semi-bold px-1">
+                    55,9937,032
+                  </h3>
+
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M14 7.77778V15H1V2H7.5"
+                      stroke="white"
+                      stroke-width="1.5"
+                    />
+                    <path
+                      d="M15 1V0.25H15.75V1H15ZM6.53033 10.5303C6.23744 10.8232 5.76256 10.8232 5.46967 10.5303C5.17678 10.2374 5.17678 9.76256 5.46967 9.46967L6.53033 10.5303ZM14.25 5.64286V1H15.75V5.64286H14.25ZM15 1.75H9.64286V0.25H15V1.75ZM15.5303 1.53033L6.53033 10.5303L5.46967 9.46967L14.4697 0.46967L15.5303 1.53033Z"
+                      fill="white"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div className="gap-2 p-6 border-[0.5px] border-neutral-700 w-96 rounded-lg mt-9">
+              <div className="flex">
+                <h3 className="text-white font-bold text-xl">Current result</h3>
+              </div>
+              <div className="mt-3">
+                <div className="flex justify-between">
+                  <h3 className="text-white">For</h3>
+                  <h3 className="text-white">1.3K VOTES 99.53%</h3>
+                </div>
+                <div className="w-full h-3 rounded-3xl bg-neutral-700">
+                  <p className="w-[98.53%] h-3 bg-white rounded-3xl"></p>
+                </div>
+              </div>
+              <div className="mt-3">
+                <div className="flex justify-between">
+                  <h3 className="text-white">Abstain</h3>
+                  <h3 className="text-white">6 VOTES 0.47%</h3>
+                </div>
+                <div className="w-full h-3 rounded-3xl bg-neutral-700">
+                  <p className="w-[1%] h-3 bg-white rounded-3xl"></p>
+                </div>
+              </div>
+              <div className="mt-3">
+                <div className="flex justify-between">
+                  <h3 className="text-white">Against</h3>
+                  <h3 className="text-white">0 VOTES 0%</h3>
+                </div>
+                <div className="w-full h-3 rounded-3xl bg-neutral-700">
+                  {/* <p className="w-[98.53%] h-3 bg-white rounded-3xl"></p> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DAODetails;
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,42 +1,96 @@
-"use client"
+"use client";
 import { useState } from "react";
-import { useRouter } from 'next/navigation'
-import Link from 'next/link';
-import Image from 'next/image';
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
 import { useTheme } from "@/app/context/ThemeProvider";
 
 const Header = () => {
   const { theme, toggleTheme } = useTheme();
-  
+  const router = useRouter();
+
   return (
     <div className="mt-2">
-      <div className={`fixed top-0 z-50 navbar ${theme === 'dark' ? 'bg-[#0e0c15]/80' : 'bg-base-100'} justify-between py-4`}>
+      <div
+        className={`fixed top-0 z-50 navbar ${
+          theme === "dark" ? "bg-[#0e0c15]/80" : "bg-base-100"
+        } justify-between py-4`}
+      >
         <div className="px-2 gap-2 items-center">
           <div className="cursor-pointer flex items-center">
-             <Image src={"/logo02.png"} objectFit="contain" width={32} height={32} alt="logo" />
-            <a className="lg:text-2xl text-xl px-2 font-bold font-poppins">Markkinat</a>
-          </div>  
+            <Image
+              src={"/logo02.png"}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+            <a className="lg:text-2xl text-xl px-2 font-bold font-poppins">
+              Markkinat
+            </a>
+          </div>
           <div className={`form-control hidden md:flex`}>
-            <label className={`${theme === 'dark' ? 'bg-[#0e0c15]/10' : 'bg-base-100'} input input-bordered flex items-center gap-1 md:w-auto xl:w-[450px] 2xl:w-[650px] rounded-2xl`}>
+            <label
+              className={`${
+                theme === "dark" ? "bg-[#0e0c15]/10" : "bg-base-100"
+              } input input-bordered flex items-center gap-1 md:w-auto xl:w-[450px] 2xl:w-[650px] rounded-2xl`}
+            >
               <svg
-                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-6 h-6
-                opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" />
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-6 h-6
+                opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
               </svg>
-                <input type="text" className="grow text-sm" placeholder="Search Item Here" />
+              <input
+                type="text"
+                className="grow text-sm"
+                placeholder="Search Item Here"
+              />
             </label>
           </div>
         </div>
-        <div className="px-2 items-center">      
+        <div className="px-2 items-center">
           <label className="cursor-pointer grid pr-2 place-items-center">
-            <input type="checkbox"
+            <input
+              type="checkbox"
               className="toggle bg-base-content row-start-1 col-start-1 col-span-2"
               onChange={toggleTheme}
             />
             <svg
-              className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+              className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="12" cy="12" r="5" />
+              <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
             </svg>
             <svg
-              className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+              className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
             </svg>
           </label>
           <ul className="hidden lg:flex menu menu-horizontal pr-2 flex-nowrap">
@@ -44,43 +98,95 @@ const Header = () => {
               <details>
                 <summary>Explore</summary>
                 <ul className="p-2">
-                  <li><a>Collections</a></li>
-                  <li><a>MKNDAO</a></li>
-                  <li><a>Hot Bids</a></li>
+                  <li>
+                    <a>Collections</a>
+                  </li>
+                  <li>
+                    <a>MKNDAO</a>
+                  </li>
+                  <li>
+                    <a>Hot Bids</a>
+                  </li>
                 </ul>
               </details>
             </li>
-            <li><a>Portfolio</a></li>
-            <li><a>DAO</a></li>
+            <li>
+              <a>Portfolio</a>
+            </li>
+            <li onClick={() => router.push("/DAO")}>
+              <a>DAO</a>
+            </li>
           </ul>
           <div className="flex gap-4 items-center">
-            <a className="hidden lg:flex border-secondary rounded-2xl text-sm">Mint</a>
-            <a className={`btn hidden  hover:bg-transparent lg:flex rounded-2xl ${theme === 'dark' ? 'bg-[#220f2c]/80 hover:text-white' : 'btn-neutral hover:text-black'} text-sm`}>Connect</a>
+            <a className="hidden lg:flex border-secondary rounded-2xl text-sm">
+              Mint
+            </a>
+            <a
+              className={`btn hidden  hover:bg-transparent lg:flex rounded-2xl ${
+                theme === "dark"
+                  ? "bg-[#220f2c]/80 hover:text-white"
+                  : "btn-neutral hover:text-black"
+              } text-sm`}
+            >
+              Connect
+            </a>
           </div>
 
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
               <li>
                 <a>Explore</a>
                 <ul className="p-2">
-                  <li><a>Collections</a></li>
-                  <li><a>MKNDAO</a></li>
-                  <li><a>Hot Bids</a></li>
+                  <li>
+                    <a>Collections</a>
+                  </li>
+                  <li>
+                    <a>MKNDAO</a>
+                  </li>
+                  <li>
+                    <a>Hot Bids</a>
+                  </li>
                 </ul>
               </li>
-              <li><a>Portfolio1</a></li>
-              <li><a>DAO</a></li>
-              <li><a>Mint</a></li>
-              <li><a className="btn btn-accent rounded-2xl hover:text-white hover:bg-transparent text-sm">Connect</a></li>       
+              <li>
+                <a>Portfolio1</a>
+              </li>
+              <li>
+                <a>DAO</a>
+              </li>
+              <li>
+                <a>Mint</a>
+              </li>
+              <li>
+                <a className="btn btn-accent rounded-2xl hover:text-white hover:bg-transparent text-sm">
+                  Connect
+                </a>
+              </li>
             </ul>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
