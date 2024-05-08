f98f59a88b613f70ef64823d9cca13b8265a6f2b
Mona Lisarogue-in-anarchy <nehemiaheje@gmail.com>
2024-05-08 23:13:42 +0100

    partial implementation of top collection

--- app/top-collection/page.tsx
+++ app/top-collection/page.tsx
@@ -1,14 +1,19 @@
-import { TopCollection } from '@/components/shared/TopCollection'
-import React from 'react'
+"use client";
+import { TopCollection } from "@/components/shared/TopCollection";
+import { useEffect, useState } from "react";
+import useGetAllListings from "../hooks/useGetAllListings";
 
 const Home = () => {
+  const listings = useGetAllListings();
+  console.log(listings);
+
   return (
-    <div className='w-11/12 m-auto'>
-      <div className='py-16'>
-        <TopCollection />
+    <div className="w-11/12 m-auto">
+      <div className="py-16">
+        {listings.length == 0 ? <p>No collections yet</p> : <TopCollection />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
