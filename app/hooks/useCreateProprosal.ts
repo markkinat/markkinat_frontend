07abda4aaf34a797b47cef641495a874026f720e
okste1234 <okste1234@gmail.com>
2024-05-09 13:35:16 +0100

    CHORE: proposal update

--- app/hooks/useCreateProprosal.ts
+++ app/hooks/useCreateProprosal.ts
@@ -2,6 +2,7 @@ import { getDAOContract } from '@/utils/constants/contracts';
 import { getProvider, isSupportedChain } from '@/utils/constants/providers';
 import { useWeb3ModalAccount, useWeb3ModalProvider } from '@web3modal/ethers/react';
 import { ethers } from 'ethers';
+import { useRouter } from 'next/navigation';
 import React, { useCallback } from 'react'
 import { toast } from 'react-toastify';
 
@@ -9,17 +10,18 @@ import { toast } from 'react-toastify';
 const useCreateProprosal = () => {
  const { walletProvider } = useWeb3ModalProvider();
   const { address, chainId } = useWeb3ModalAccount();
+  const router = useRouter()
 
   return useCallback(async (name: string, deadline: any, desc: string) => {
       const readWriteProvider = getProvider(walletProvider);
       const signer = await readWriteProvider.getSigner();
-     
 
       const contract = getDAOContract(signer);
 
       try {
         if (!isSupportedChain(chainId)) return toast.error("No Wallet Connected or Wrong Network");
         const transaction = await contract.createProposal(
+          // address,
           name,
           deadline,
           desc
@@ -30,13 +32,14 @@ const useCreateProprosal = () => {
         console.log("receipt: ", receipt);
 
         if (receipt.status) {
+          router.push("/DAO")
           return toast.success("Proposal Created!");
         }
         toast.error("Failed!");
       } catch (error) {
         console.log("error writing to contract :", error);
       }
-    }, [chainId, walletProvider]);
+    }, [address, chainId, router, walletProvider]);
 }
 
 export default useCreateProprosal
