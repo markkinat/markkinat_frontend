2c8b108f152800f4d98535a0b6b434304649416c
okste1234 <okste1234@gmail.com>
2024-06-03 11:32:50 +0100

    UPDATE: rpc update

--- connection/index.ts
+++ connection/index.ts
@@ -1,37 +1,37 @@
 "use client"
 import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react";
 
-export const SUPPORTED_CHAIN = 11155111;
+export const SUPPORTED_CHAIN = 4202;
 // export const SUPPORTED_CHAIN = 31337;
 
 
-const sepolia = {
+const LiskSepolia = {
     chainId: SUPPORTED_CHAIN,
-    name: "Sepolia",
+    name: "Lisk Sepolia Testnet",
     currency: "ETH",
-    explorerUrl: "https://sepolia.etherscan.io",
+    explorerUrl: "https://sepolia-blockscout.lisk.com",
     rpcUrl: process.env.NEXT_PUBLIC_rpc_url || "",
 };
 
-const local = {
-    chainId: SUPPORTED_CHAIN,
-    name: "local",
-    currency: "OKSTE",
-    explorerUrl: "https://sepolia.etherscan.io",
-    rpcUrl: process.env.NEXT_PUBLIC_rpc_url || "",
-};
+// const local = {
+//     chainId: SUPPORTED_CHAIN,
+//     name: "local",
+//     currency: "OKSTE",
+//     explorerUrl: "https://sepolia.etherscan.io",
+//     rpcUrl: process.env.NEXT_PUBLIC_rpc_url || "",
+// };
 
 const metadata = {
-    name: "My Website",
-    description: "My Website description",
-    url: "https://mywebsite.com", // origin must match your domain & subdomain
+    name: "Markkinat",
+    description: "A decentralized application that combines an NFT marketplace with on-chain voting capabilities, empowering community-driven governance and economic activity through blockchain technology",
+    url: "https://markkinat-frontend.vercel.app", 
     icons: ["https://avatars.mywebsite.com/"],
 };
 
 export const configureWeb3Modal = () =>
     createWeb3Modal({
         ethersConfig: defaultConfig({ metadata }),
-        chains: [sepolia],
+        chains: [LiskSepolia],
         projectId: process.env.NEXT_PUBLIC_PROJECT_ID || "",
-        enableAnalytics: false, // Optional - defaults to your Cloud configuration
+        enableAnalytics: false, 
     });
